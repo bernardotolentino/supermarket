036f3999a6aa83a17b302bd2c9186d871572dfb3
Bernardo <bernardo.henrique@gmail.com>
2023-08-22 21:42:58 -0300

    frontEnd up

--- example-app/routes/api.php
+++ example-app/routes/api.php
@@ -1,8 +1,9 @@
 <?php
-
+ 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
+ 
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +14,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+ 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+ 
+Route::get('users', [UserController::class, 'index']);
+Route::get('users/{id}', [UserController::class, 'show']); 
+Route::post('addnew', [UserController::class, 'store']); 
+Route::put('usersupdate/{id}', [UserController::class, 'update']);
+Route::delete('usersdelete/{id}', [UserController::class, 'destroy']);
